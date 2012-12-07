5ae337d62a1a9f709a807d97ae2424491ce3620e
Maksim Berjoza <torbjon@gmail.com>
2012-12-07 15:04:07 +0200

    scss

--- site.rb
+++ site.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require 'sass'
+
+get '/css/:name.css' do |name|
+  # require './views/scss/bourbon/lib/bourbon.rb'
+  content_type 'text/css', :charset => 'utf-8'
+  scss name.to_sym, layout: false, views: "#{settings.root}/public/css"
+end
 
 get '/' do
   erb :index
@@ -7,3 +14,4 @@ end
 get '/about' do
   erb :about
 end
+
