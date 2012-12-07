43dc2b7b6c55f1c584203d085dfcc50ebe648e40
Maksim Berjoza <torbjon@gmail.com>
2012-12-08 01:09:55 +0200

    styling

--- site.rb
+++ site.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'sass'
 
 get '/css/:name.css' do |name|
-  require './views/scss/bourbon/lib/bourbon.rb'
   content_type 'text/css', charset: 'utf-8'
   scss name.to_sym, layout: false, views: "#{settings.root}/public/css"
 end
