5765dbb7473cf32028081a0fcb1f7a98b73d203f
Maksim Berjoza <torbjon@gmail.com>
2012-12-12 13:55:11 +0200

    menu logick

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'sass'
 
+before do
+  response.headers['X-UA-Compatible'] = 'IE=edge,chrome=1'
+end
+
 get '/css/:name.css' do |name|
   content_type 'text/css', charset: 'utf-8'
   scss name.to_sym, layout: false, views: "#{settings.root}/public/css"
-end
-
-before do
-  response.headers['X-UA-Compatible'] = 'IE=edge,chrome=1'
 end
 
 get '/' do
