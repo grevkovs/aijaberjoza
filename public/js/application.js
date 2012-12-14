4724714512c5a35b0abf026c04c804d651954649
Maksim Berjoza <torbjon@gmail.com>
2012-12-14 19:38:49 +0200

    mixpanel tracker

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,7 @@ $('a.gmodal').modal({opacity: 0.85});
 
 $("#refresh_recommendation").on('click', function(event) {
   event.preventDefault();
+  mixpanel.track("Reload recommendations");
   recomendations = _.shuffle(recomendations)
   $(this).closest(".quotes").find('.quote').html(recomendations[0].text);
   $(this).closest(".quotes").find('.signature').html(recomendations[0].name);
